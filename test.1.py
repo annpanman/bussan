c34a9e9d23ee41f55349f0003321c5dce7699f4e
西条 友貴 <annpanman.baikinman@gmail.com>
2016-01-23 12:40:57 +0900

    作業２

--- test.1.py
+++ test.1.py
@@ -3,3 +3,21 @@ for i in range(1,5):
     for j in range(1,14):
         a+=[[i,j]]
 
+b=[b1,b2]
+c=[c1,c2]
+d=[d1,d2]
+e=[e1,e2]
+f=[f1,f2]
+
+b1=int(input())
+b2=int(input())
+c1=int(input())
+c2=int(input())
+d1=int(input())
+d2=int(input())
+e1=int(input())
+e2=int(input())
+f1=int(input())
+f2=int(input())
+
+g=[b,c,d,e,f]
